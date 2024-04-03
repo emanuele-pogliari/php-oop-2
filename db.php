2befd548f854175e2e392171ee927a9929ae2b35
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-03 21:49:48 +0200

    style completed

--- db.php
+++ db.php
@@ -11,9 +11,9 @@ require "./Models/Games.php";
 $product1Category = new Category("cani");
 
 $product1 = new Foods(
-    "Dagel alfa cane pollo da 20 kg",
-    "Top Cat Premium Carne Arrosto è un cibo secco per gatti in grado di fornire al tuo gatto un'alimentazione completa, realizzata per soddisfare il requisito nutrizionale dei gatti che svolgono un intenso lavoro fisico (agilità, caccia, esposizione, allattamento). Queste crocchette per gatti sono prodotte in modo particolare per fornire un'adeguata digestione, soprattutto dei grassi (energia pronta ad essere utilizzata). Caratteristiche principali di Top Cat: adatto per gatti sterilizzati, aiuta ad eliminare i peli ingeriti, aiuta lo sviluppo muscolare, sano, gustoso e appetibile, completo e digeribile, ricco di antiossidanti, senza coloranti aggiunti, Made in Italy. La ricetta è preparata con carne disidratata e con solo grassi animali di altissima qualità, che oltre all'immediato apporto energetico, aumentano l'appetibilità ed esaltano la lucentezza del pelo. Top Cat è un prodotto dal valore nutrizionale elevato, in grado di offrire una alimentazione completa e bilanciata grazie all’apporto ottimale di proteine, grassi, carboidrati, vitamine e minerali utili al sostentamento ed il benessere del tuo gatto.",
-    "https://www.zooexpert.it/15850-thickbox_default/dagel-alfa-gatto-pollo-da-20-kg.jpg",
+    "Purina veterinary diet cane",
+    "Purina pro plan è un alimento dietetico completo per cuccioli e cani adulti, altamente digeribile, formulato con basso contenuto di fibre per la compensazione della cattiva digestione e in caso di insufficienza pancreatica esocrina.",
+    "https://www.zooexpert.it/15338-large_default/purina-veterinary-diet-cane-en-gastrointestinal-da-12-kg.jpg",
     "53,80 €",
     $product1Category,
     "2 kg",
@@ -23,20 +23,20 @@ $product1 = new Foods(
 $product2Category = new Category("gatti");
 
 $product2 = new Foods(
-    "Dagel alfa gatto pollo da 10 kg",
-    "Top Cat Premium Carne Arrosto è un cibo secco per gatti in grado di fornire al tuo gatto un'alimentazione completa, realizzata per soddisfare il requisito nutrizionale dei gatti che svolgono un intenso lavoro fisico (agilità, caccia, esposizione, allattamento). Queste crocchette per gatti sono prodotte in modo particolare per fornire un'adeguata digestione, soprattutto dei grassi (energia pronta ad essere utilizzata). Caratteristiche principali di Top Cat: adatto per gatti sterilizzati, aiuta ad eliminare i peli ingeriti, aiuta lo sviluppo muscolare, sano, gustoso e appetibile, completo e digeribile, ricco di antiossidanti, senza coloranti aggiunti, Made in Italy. La ricetta è preparata con carne disidratata e con solo grassi animali di altissima qualità, che oltre all'immediato apporto energetico, aumentano l'appetibilità ed esaltano la lucentezza del pelo. Top Cat è un prodotto dal valore nutrizionale elevato, in grado di offrire una alimentazione completa e bilanciata grazie all’apporto ottimale di proteine, grassi, carboidrati, vitamine e minerali utili al sostentamento ed il benessere del tuo gatto.",
-    "https://www.zooexpert.it/15850-thickbox_default/dagel-alfa-gatto-pollo-da-20-kg.jpg",
-    "34,80 €",
+    "Agras schesir gatto dry carnivore aringa e salmone",
+    "Agras schesir gatto dry born carnivore aringa e arancia, sono le uniche crocchette cotte al forno a bassa temperatura, per conservare vitamine e minerali.",
+    "https://www.zooexpert.it/15840-large_default/agras-schesir-gatto-dry-carnivore-aringa-e-salmone-da-125-kg.jpg",
+    "19,19 €",
     $product2Category,
-    "1.5 kg",
-    "Agnello, riso",
+    "1.25 kg",
+    "Aringa, Salmone",
 );
 
 $product3Category = new Category("gatti");
 
 $product3 = new Games(
-    "VITAKRAFT GATTO TIRAGRAFFI MULTIPIANO CON AMACA E CUCCETTE GRIGIO",
-    "Il tiragraffi multipiano di Vitakraft è la soluzione ideale per far giocare il tuo gatto grazie ai vari ripiani e ai giochi. Le cuccette e l’amaca sono il posto ideale dove il gatto può riposare, mentre sulle colonne può soddisfare il naturale bisogno di farsi le unghie, allontanandolo così da divani, tende e tappeti. Il tiragraffi è rivestito in morbido peluche e le colonnine sono ricoperte di corda di Sisal, che non danneggia le unghie. In colore grigio.",
+    "Vitakraft Tiragraffi Multipiano con amaca e cuccette",
+    "Il tiragraffi multipiano di Vitakraft è la soluzione ideale per far giocare il tuo gatto grazie ai vari ripiani e ai giochi. Le cuccette e l’amaca sono il posto ideale dove il gatto può riposare, mentre sulle colonne può soddisfare il naturale bisogno di farsi le unghie, allontanandolo così da divani, tende e tappeti.",
     "https://www.zooexpert.it/15547-large_default/vitakraft-gatto-tiragraffi-multipiano-con-amaca-e-cuccette-grigio.jpg",
     "106,27 €",
     $product3Category,
@@ -46,8 +46,8 @@ $product3 = new Games(
 $product4Category = new Category("cani");
 
 $product4 = new Games(
-    "PALLA GOMMA DURA LARGE",
-    "Questa pallina per cani in gomma dura è un utile gioco antistress per far divertire il vostro animale durante la giornata. Realizzata con un materiale particolarmente resistente,  è perfetta se il vostro amico ha l'abitudine di mordere e prendere in bocca il gioco. Prodotta con materiali assolutamente sicuri e di prima scelta, questa palla-gioco è disponibile in quattro diverse grandezze di diametro per adattarsi a tutte le taglie di cani ed è decorata con una piacevole fantasia astratta multicolore. Un simpatico regalo per il vostro adorato peloso!",
+    "Palla gomma dura large",
+    "Questa pallina per cani in gomma dura è un utile gioco antistress per far divertire il vostro animale durante la giornata. Realizzata con un materiale particolarmente resistente,  è perfetta se il vostro amico ha l'abitudine di mordere e prendere in bocca il gioco.",
     "https://www.zooexpert.it/1181-large_default/palla-gomma-dura-large.jpg",
     "7,52 €",
     $product4Category,
@@ -58,7 +58,7 @@ $product5Category = new Category("cani");
 
 $product5 = new Beds(
     "Leopet cane cuccia rodi verde reversibile",
-    " La Cuccia Rodi Reversibile Leopet è realizzata in tessuto tecnico idrorepellente Oxford ed è particolarmente indicata per i Pet Lover che si stancano facilmente della stessa cuccia. Grazie alla sua reversibilità, presenta due lati con colorazioni diverse. La cuccia ha una forma rettangolare, è metà in tessuto tecnico in tinta unita verde con trama a nido d’ape waterproof e metà in tinta unita verde. La cuccia è dotata di cuscino estraibile e base antiscivolo ed è imbottita con morbida fibra di poliestere.",
+    " La Cuccia Rodi Reversibile Leopet è realizzata in tessuto tecnico idrorepellente Oxford ed è particolarmente indicata per i Pet Lover che si stancano facilmente della stessa cuccia. Grazie alla sua reversibilità, presenta due lati con colorazioni diverse.",
     "https://www.zooexpert.it/15575-large_default/leopet-cane-cuccia-rodi-verde-60x70-cm-reversibile.jpg",
     "34,77 €",
     $product5Category,
@@ -69,7 +69,7 @@ $product6Category = new Category("gatti");
 
 $product6 = new Beds(
     "Ferribiella gatto igloo gatto nuvoletta rosa",
-    "Top Cat Premium Carne Arrosto è un cibo secco per gatti in grado di fornire al tuo gatto un'alimentazione completa, realizzata per soddisfare il requisito nutrizionale dei gatti che svolgono un intenso lavoro fisico (agilità, caccia, esposizione, allattamento). Queste crocchette per gatti sono prodotte in modo particolare per fornire un'adeguata digestione, soprattutto dei grassi (energia pronta ad essere utilizzata). Caratteristiche principali di Top Cat: adatto per gatti sterilizzati, aiuta ad eliminare i peli ingeriti, aiuta lo sviluppo muscolare, sano, gustoso e appetibile, completo e digeribile, ricco di antiossidanti, senza coloranti aggiunti, Made in Italy. La ricetta è preparata con carne disidratata e con solo grassi animali di altissima qualità, che oltre all'immediato apporto energetico, aumentano l'appetibilità ed esaltano la lucentezza del pelo. Top Cat è un prodotto dal valore nutrizionale elevato, in grado di offrire una alimentazione completa e bilanciata grazie all’apporto ottimale di proteine, grassi, carboidrati, vitamine e minerali utili al sostentamento ed il benessere del tuo gatto.",
+    "Top Cat Premium Carne Arrosto è un cibo secco per gatti in grado di fornire al tuo gatto un'alimentazione completa, realizzata per soddisfare il requisito nutrizionale dei gatti che svolgono un intenso lavoro fisico (agilità, caccia, esposizione, allattamento).",
     "https://www.zooexpert.it/14396-large_default/ferribiella-gatto-igloo-gatto-nuvoletta-50-x-50-cm-rosa.jpg",
     "49,80 €",
     $product6Category,
--- index.php
+++ index.php
@@ -6,26 +6,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="stylesheet" href="css/style.css">
     <title>Oop 2</title>
 </head>
 
 <body>
 
-
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img class="my-logo" src="./img/pngwing.com.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cane</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gatto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pesci</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Piccoli animali</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container">
-        <h1>Pet e-commerce</h1>
-        <div class="row">
+        <div class="row mx-auto justify-content-center">
             <?php
             foreach ($products as $product) {
             ?>
-                <div class='card col-3 m-3'>
-                    <img src='<?= $product->image ?>'>
-                    <h3> <?= $product->title ?></h3>
-                    <p> <?= $product->description ?></p>
-                    <p> <?= $product->price ?></p>
-                    <p><?= is_a($product, 'Foods') ? $product->quantity : '' ?></p>
+                <div class='card col-3 m-3 p-0 border-0 rounded-3 overflow-hidden  '>
+                    <div class="my-badge">Hot</div>
+                    <img class="pb-2" src='<?= $product->image ?>'>
+                    <div class="card-body p-2">
+                        <h3 class="title"><?= $product->title ?></h3>
+                        <p class="product-details"><?= $product->description ?></p>
+                        <p class="product-food-quantity"><?= is_a($product, 'Foods') ? 'Quantità: ' . $product->quantity : '' ?></p>
+                        <p class="product-food-ingredients"><?= is_a($product, 'Foods') ? 'Ingredienti: ' . $product->ingredients : '' ?></p>
+                        <p class="product-games-material"><?= is_a($product, 'Games') ? 'Composizione: ' . $product->material : '' ?></p>
+                        <p class="product-beds-size"><?= is_a($product, 'Beds') ? 'Dimensioni: ' . $product->size : '' ?></p>
+                        <p class="product-price">Prezzo: <?= $product->price ?></p>
+                    </div>
                 </div>
             <?php
             }
@@ -33,6 +68,7 @@
         </div>
 
     </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
